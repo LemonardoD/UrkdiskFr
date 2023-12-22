948c9ff508425e9c867cb031fd51d3159ac39edc
David Hordiienko <neonz96@mail.ru>
2023-12-22 13:45:09 +0200

    search by car nav change

--- src/components/carSelector.svelte
+++ src/components/carSelector.svelte
@@ -33,8 +33,8 @@
         if (selectedBrand.length === 0|| selectedModel.length === 0 || selectedYear.length === 0) {
             fieldsError = true
         } else {
-            await goto(`/rims-by-car?brand=${selectedBrand}&model=${selectedModel}&year=${selectedYear}&rimBrand=${rimBrand}`)
-            location.reload()
+            if(typeof window !== 'undefined')
+            window.location.href=`/rims-by-car?brand=${selectedBrand}&model=${selectedModel}&year=${selectedYear}&rimBrand=${rimBrand}`
         }
     }
 
